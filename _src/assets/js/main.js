10daa8687afcabfe3b884b3abd40841a64fa0023
patrozada <patriciarozada@gmail.com>
2019-05-03 18:02:55 +0200

    Add parallax. Need fixing in mobile

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,16 @@
 'use strict';
+//PARALLAX
 
-console.log('>> Ready :)');
+const midImgHigh = document.querySelector('.mid--img__high');
+const midImgLow = document.querySelector('.mid--img__low');
+const bottomImg = document.querySelector('.bottom--img');
+
+function handleScrollY(){
+  if(window.matchMedia('min-width:768px')){
+    let scrollY = window.pageYOffset;
+    midImgHigh.style.top = 100+(scrollY*0.5)+'px';
+    midImgLow.style.top = 300+(scrollY*0.6)+'px';
+    bottomImg.style.top = 200+(scrollY*0.75)+'px';
+  }
+}
+window.addEventListener('scroll', handleScrollY);
