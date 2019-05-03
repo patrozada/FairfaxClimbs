091718d577201ecc7d1cff565bb0f6eaf20eb6b2
patrozada <patriciarozada@gmail.com>
2019-05-03 18:59:59 +0200

    Fix parallax issues

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -6,11 +6,11 @@ const midImgLow = document.querySelector('.mid--img__low');
 const bottomImg = document.querySelector('.bottom--img');
 
 function handleScrollY(){
-  if(window.matchMedia('min-width:768px')){
+  if(window.outerWidth >= 768){
     let scrollY = window.pageYOffset;
-    midImgHigh.style.top = 100+(scrollY*0.5)+'px';
-    midImgLow.style.top = 300+(scrollY*0.6)+'px';
-    bottomImg.style.top = 200+(scrollY*0.75)+'px';
+    midImgHigh.style.top = 150+(scrollY*0.5)+'px';
+    midImgLow.style.top = 350+(scrollY*0.6)+'px';
+    bottomImg.style.top = 250+(scrollY*0.75)+'px';
   }
 }
 window.addEventListener('scroll', handleScrollY);
